e4d5689ff4271f22fe5d33c32e282fb70be478cc
xyz <hsyntr33@gmail.com>
2023-07-08 14:53:40 +0300

    Bool updated

--- bool/src/main/java/com/tr/hsyn/bool/Any.java
+++ bool/src/main/java/com/tr/hsyn/bool/Any.java
@@ -1,63 +1,91 @@
 package com.tr.hsyn.bool;
 
 
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 
 /**
  * <h1>Any</h1>
  * <p>
- * Taban sınıfı.
+ * Provides more functional approach to the nullable objects.
  *
  * @param <T>
  * @author hsyn 13 Haziran 2022 Pazartesi 21:05
  */
+@FunctionalInterface
 public interface Any<T> {
-
-    @Nullable
-    T getObject();
-
-    default boolean isNull() {
-
-        return getObject() == null;
-    }
-
-    default boolean isNotNull() {
-
-        return getObject() != null;
-    }
-
-    default Any<T> ifNotNull(Runnable runnable) {
-
-        if (isNotNull()) runnable.run();
-        return this;
-    }
-
-    default Any<T> ifNotNull(Consumer<T> consumer) {
-
-        if (isNotNull()) consumer.accept(getObject());
-        return this;
-    }
-
-    default Any<T> ifNull(Runnable runnable) {
-
-        if (isNull()) runnable.run();
-        return this;
-    }
-
-    default Any<T> orElse(Any<T> value) {
-
-        return isNull() ? value : this;
-    }
-
-    default Any<T> orElse(Any<T> value, Consumer<Any<T>> consumer) {
-
-        if (isNotNull()) consumer.accept(this);
-        else consumer.accept(value);
-
-        return this;
-    }
-
+	
+	/**
+	 * The held object.
+	 *
+	 * @return object
+	 */
+	@Nullable T getObject();
+	
+	/**
+	 * @return {@code true} if the object is null.
+	 */
+	default boolean isNull() {
+		
+		return getObject() == null;
+	}
+	
+	/**
+	 * @return {@code true} if the object is not null.
+	 */
+	default boolean isNotNull() {
+		
+		return getObject() != null;
+	}
+	
+	/**
+	 * Calls the runnable if the object is null.
+	 *
+	 * @param runnable runnable
+	 * @return this object
+	 */
+	default Any<T> ifNull(Runnable runnable) {
+		
+		if (isNull()) runnable.run();
+		return this;
+	}
+	
+	/**
+	 * Calls the runnable if the object is not null.
+	 *
+	 * @param runnable runnable
+	 * @return this object
+	 */
+	default Any<T> ifNotNull(Runnable runnable) {
+		
+		if (isNotNull()) runnable.run();
+		return this;
+	}
+	
+	/**
+	 * Calls the consumer if the object is not null.
+	 *
+	 * @param consumer consumer
+	 * @return this object
+	 */
+	default Any<T> ifNotNull(Consumer<T> consumer) {
+		
+		if (isNotNull()) consumer.accept(getObject());
+		return this;
+	}
+	
+	default boolean equalTo(Object o) {
+		
+		return o instanceof Any && Objects.equals(getObject(), ((Any<?>) o).getObject());
+	}
+	
+	@NotNull
+	static <T> Any<T> of(T value) {
+		
+		return () -> value;
+	}
 }
--- bool/src/main/java/com/tr/hsyn/bool/Bool.java
+++ bool/src/main/java/com/tr/hsyn/bool/Bool.java
@@ -7,46 +7,132 @@ import java.util.Objects;
 
 
 public class Bool implements Any<Boolean> {
-
-    public static final     Bool    FALSE = new Bool();
-    public static final     Bool    TRUE  = new Bool(true);
-    public static final     Bool    NONE  = new Bool(null);
-    @Nullable private final Boolean value;
-
-    public Bool() {
-
-        value = false;
-    }
-
-    public Bool(@Nullable Boolean value) {
-
-        this.value = value;
-    }
-
-    /**
-     * @return Saklanan değer {@code null} ise false, yada saklanan değer
-     */
-    public boolean bool() {
-
-        return value != null && value;
-    }
-
-    @Nullable
-    @Override
-    public Boolean getObject() {
-
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-
-        return o instanceof Bool && Objects.equals(value, ((Bool) o).value);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(value);
-    }
+	
+	public static final     Bool    FALSE = new Bool();
+	public static final     Bool    TRUE  = new Bool(true);
+	public static final     Bool    NONE  = new Bool(null);
+	@Nullable private final Boolean value;
+	
+	/**
+	 * Creates a new Bool instance.
+	 */
+	public Bool() {
+		
+		value = null;
+	}
+	
+	/**
+	 * Creates a new Bool instance with the given value.
+	 *
+	 * @param value value
+	 */
+	public Bool(@Nullable Boolean value) {
+		
+		this.value = value;
+	}
+	
+	@Nullable
+	@Override
+	public Boolean getObject() {
+		
+		return value;
+	}
+	
+	@Override
+	public int hashCode() {
+		
+		return Objects.hash(value);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+		return o instanceof Bool && Objects.equals(value, ((Bool) o).value);
+	}
+	
+	/**
+	 * @return {@code false} if the held value is null, or the held value.
+	 */
+	public boolean bool() {
+		
+		return value != null && value;
+	}
+	
+	/**
+	 * Tests if the held value is true.
+	 *
+	 * @return {@code false} if the held value is null,
+	 * 		or {@code true} if the held value is true.
+	 */
+	public boolean isTrue() {
+		
+		return bool();
+	}
+	
+	/**
+	 * Tests if the held value is false.
+	 *
+	 * @return {@code true} if the held value is null,
+	 * 		or if the held value is false.
+	 */
+	public boolean isFalse() {
+		
+		return !bool();
+	}
+	
+	/**
+	 * Creates a new Bool instance.
+	 *
+	 * @param value value
+	 * @return Bool instance
+	 */
+	public static Bool of(boolean value) {
+		
+		return value ? TRUE : FALSE;
+	}
+	
+	/**
+	 * Creates a new Bool instance.
+	 *
+	 * @param value value
+	 * @return Bool instance
+	 */
+	public static Bool of(@Nullable Boolean value) {
+		
+		return value != null ? of(value.booleanValue()) : NONE;
+	}
+	
+	/**
+	 * Creates a new Bool instance.
+	 *
+	 * @param value value. Non-zero value means true.
+	 * @return Bool instance
+	 */
+	public static Bool of(int value) {
+		
+		return of(value != 0);
+	}
+	
+	/**
+	 * Creates a new Bool instance.
+	 *
+	 * @param value value. Non-zero value means true.
+	 * @return Bool instance
+	 */
+	public static Bool of(long value) {
+		
+		return of(value != 0L);
+	}
+	
+	/**
+	 * Creates a new Bool instance.
+	 *
+	 * @param value value. Non-null value means true.
+	 * @param <T>   type of object
+	 * @return Bool instance
+	 */
+	public static <T> Bool of(T value) {
+		
+		return of(value != null);
+	}
 }
