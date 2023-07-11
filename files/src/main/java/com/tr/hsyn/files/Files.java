6313f974d2b436aee9410e533eb3eabd94992796
xyz <hsyntr33@gmail.com>
2023-07-11 08:57:51 +0300

    update codes

--- files/src/main/java/com/tr/hsyn/files/Files.java
+++ files/src/main/java/com/tr/hsyn/files/Files.java
@@ -1,5 +1,6 @@
 package com.tr.hsyn.files;
 
+
 import android.content.Context;
 import android.media.MediaMetadataRetriever;
 import android.util.Log;
@@ -17,263 +18,263 @@ import java.util.List;
 import java.util.Locale;
 
 
-
-public class Files{
-   
-   public static long getDuration(String filePath){
-      
-      long fileDuration = -60L;
-      
-      try{
-         
-         MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
-         metaRetriever.setDataSource(filePath);
-         
-         String duration = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-         
-         metaRetriever.release();
-         fileDuration = Long.parseLong(duration);
-      }
-      catch(Exception e){
-   
-         Log.d("Rehber", "Bilgi alınamadı");
-      }
-      
-      return fileDuration;
-   }
-   
-   public static String getMp3Duration(File file){
-      
-      return formatMilliSeconds(getDuration(file.getAbsolutePath()));
-   }
-   
-   public static boolean deleteFile(File file){
-      
-      if(file == null){
-   
-         Log.d("Rehber", "file = null (bu bir hata değil)");
-         return false;
-      }
-      
-      if(file.delete()){
-   
-         Log.d("Rehber", "Dosya silindi : " + file.getName());
-         return true;
-      }
-   
-      if(!file.exists()){
-   
-         Log.d("Rehber", "Dosya silinemedi çünkü böyle bir dosya yok : " + file.getName());
-      }
-      else{
-   
-         Log.d("Rehber", "Dosya silinemedi  : " + file.getName());
-      }
-      
-      return false;
-   }
-   
-   public static String formatMilliSeconds(long milliseconds){
-      
-      // Convert total duration into time
-      int hours   = (int) (milliseconds / (1000 * 60 * 60));
-      int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
-      int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
-      
-      if(hours != 0){
-         
-         return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
-      }
-      
-      return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-   }
+public class Files {
+	
+	public static long getDuration(String filePath) {
+		
+		long fileDuration = -60L;
+		
+		try {
+			
+			MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
+			metaRetriever.setDataSource(filePath);
+			
+			String duration = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+			
+			metaRetriever.release();
+			fileDuration = Long.parseLong(duration);
+		}
+		catch (Exception e) {
+			
+			Log.d("Rehber", "Bilgi alınamadı");
+		}
+		
+		return fileDuration;
+	}
+	
+	public static String getMp3Duration(File file) {
+		
+		return formatMilliSeconds(getDuration(file.getAbsolutePath()));
+	}
+	
+	public static boolean deleteFile(File file) {
+		
+		if (file == null) {
+			
+			Log.d("Rehber", "file = null (bu bir hata değil)");
+			return false;
+		}
+		
+		if (file.delete()) {
+			
+			Log.d("Rehber", "Dosya silindi : " + file.getName());
+			return true;
+		}
+		
+		if (!file.exists()) {
+			
+			Log.d("Rehber", "Dosya silinemedi çünkü böyle bir dosya yok : " + file.getName());
+		}
+		else {
+			
+			Log.d("Rehber", "Dosya silinemedi  : " + file.getName());
+		}
+		
+		return false;
+	}
+	
+	public static String formatMilliSeconds(long milliseconds) {
+		
+		// Convert total duration into time
+		int hours   = (int) (milliseconds / (1000 * 60 * 60));
+		int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
+		int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+		
+		if (hours != 0) {
+			
+			return String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
+		}
+		
+		return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+	}
 	
 	public static String formatSeconds(int seconds) {
 		
-   	return formatMilliSeconds(seconds * 1000L);
+		return formatMilliSeconds(seconds * 1000L);
 	}
 	
-   public static String getAudioFolder(Context context){
-      
-      File file = new File(context.getFilesDir(), "audio");
-      
-      if(!file.exists()){
-         
-         if(file.mkdir()){
-            
-            return file.getAbsolutePath();
-         }
-      }
-      else{
-         return file.getAbsolutePath();
-      }
-      
-      return context.getFilesDir() + "/audio";
-   }
-   
-   public static String getCallAudioFolder(Context context){
-      
-      File file = new File(context.getFilesDir(), "call");
-      
-      if(!file.exists()){
-         
-         if(file.mkdir()){
-            
-            return file.getAbsolutePath();
-         }
-      }
-      else{
-         return file.getAbsolutePath();
-      }
-      
-      return context.getFilesDir() + "/call";
-   }
-   
-   public static File getAudioFolderFile(Context context){
-      
-      File file = new File(context.getFilesDir(), "audio");
-      
-      if(!file.exists()){
-         
-         if(file.mkdir()){
-            
-            return file;
-         }
-      }
-      else{
-         return file;
-      }
-      
-      return new File(context.getFilesDir() + "/audio");
-   }
-   
-   public static File getCallAudioFolderFile(Context context){
-      
-      File file = new File(context.getFilesDir(), "call");
-      
-      if(!file.exists()){
-         
-         if(file.mkdir()){
-            
-            return file;
-         }
-      }
-      else{
-         return file;
-      }
-      
-      return new File(context.getFilesDir() + "/call");
-   }
-   
-   @Nullable
-   private static File getMailbox(Context context){
-   
-      File file = new File(context.getFilesDir(), "mailbox");
-   
-      if(!file.exists()){
-      
-         if(file.mkdir()){
-         
-            return file;
-         }
-         else{
-   
-            return null;
-         }
-      }
-      else{
-         
-         return file;
-      }
-   }
-   
-   @Nullable
-   public static File getSentbox(Context context){
-      
-      File file = new File(getMailbox(context), "sentbox");
-      
-      if(!file.exists()){
-         
-         if(file.mkdir()){
-            
-            return file;
-         }
-         else{
-   
-            return null;
-         }
-      }
-      else{
-         return file;
-      }
-   }
-   
-   @Nullable
-   public static File getOutbox(Context context){
-      
-      File file = new File(getMailbox(context), "outbox");
-      
-      if(!file.exists()){
-         
-         if(file.mkdir()){
-            
-            return file;
-         }
-         else{
-   
-            return null;
-         }
-      }
-      else{
-         return file;
-      }
-   }
-   
-   @Nullable
-   public static File getInbox(Context context){
-      
-      File file = new File(getMailbox(context), "inbox");
-      
-      if(!file.exists()){
-         
-         if(file.mkdir()){
-            
-            return file;
-         }
-         else{
-            
-            return null;
-         }
-      }
-      else{
-         return file;
-      }
-   }
-   
-   @Nullable @SuppressWarnings("ResultOfMethodCallIgnored")
-   public static String getFileContent(@NonNull File file){
-      
-      if(!file.exists()) return null;
-      
-      long len = file.length();
-      
-      byte[] byteArray = new byte[(int) len];
-      
-      try{
-         
-         FileInputStream stream = new FileInputStream(file);
-         stream.read(byteArray);
-         stream.close();
-         
-         return new String(byteArray);
-      }
-      catch(Exception ignore){}
-      
-      return null;
-   }
+	public static String getAudioFolder(Context context) {
+		
+		File file = new File(context.getFilesDir(), "audio");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file.getAbsolutePath();
+			}
+		}
+		else {
+			return file.getAbsolutePath();
+		}
+		
+		return context.getFilesDir() + "/audio";
+	}
+	
+	public static String getCallAudioFolder(Context context) {
+		
+		File file = new File(context.getFilesDir(), "call");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file.getAbsolutePath();
+			}
+		}
+		else {
+			return file.getAbsolutePath();
+		}
+		
+		return context.getFilesDir() + "/call";
+	}
+	
+	public static File getAudioFolderFile(Context context) {
+		
+		File file = new File(context.getFilesDir(), "audio");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file;
+			}
+		}
+		else {
+			return file;
+		}
+		
+		return new File(context.getFilesDir() + "/audio");
+	}
+	
+	public static File getCallAudioFolderFile(Context context) {
+		
+		File file = new File(context.getFilesDir(), "call");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file;
+			}
+		}
+		else {
+			return file;
+		}
+		
+		return new File(context.getFilesDir() + "/call");
+	}
 	
 	@Nullable
-	public static<T> File writeObjects(@NonNull File file, @NonNull List<T> objects) {
+	private static File getMailbox(Context context) {
+		
+		File file = new File(context.getFilesDir(), "mailbox");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file;
+			}
+			else {
+				
+				return null;
+			}
+		}
+		else {
+			
+			return file;
+		}
+	}
+	
+	@Nullable
+	public static File getSentBox(Context context) {
+		
+		File file = new File(getMailbox(context), "sentbox");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file;
+			}
+			else {
+				
+				return null;
+			}
+		}
+		else {
+			return file;
+		}
+	}
+	
+	@Nullable
+	public static File getOutbox(Context context) {
+		
+		File file = new File(getMailbox(context), "outbox");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file;
+			}
+			else {
+				
+				return null;
+			}
+		}
+		else {
+			return file;
+		}
+	}
+	
+	@Nullable
+	public static File getInbox(Context context) {
+		
+		File file = new File(getMailbox(context), "inbox");
+		
+		if (!file.exists()) {
+			
+			if (file.mkdir()) {
+				
+				return file;
+			}
+			else {
+				
+				return null;
+			}
+		}
+		else {
+			return file;
+		}
+	}
+	
+	@Nullable
+	@SuppressWarnings("ResultOfMethodCallIgnored")
+	public static String getFileContent(@NonNull File file) {
+		
+		if (!file.exists()) return null;
+		
+		long len = file.length();
+		
+		byte[] byteArray = new byte[(int) len];
+		
+		try {
+			
+			FileInputStream stream = new FileInputStream(file);
+			stream.read(byteArray);
+			stream.close();
+			
+			return new String(byteArray);
+		}
+		catch (Exception ignore) {}
+		
+		return null;
+	}
+	
+	@Nullable
+	public static <T> File writeObjects(@NonNull File file, @NonNull List<T> objects) {
 		
 		ObjectOutputStream out = null;
 		
@@ -289,7 +290,7 @@ public class Files{
 		}
 		finally {
 			
-			if(out != null) {
+			if (out != null) {
 				try {
 					out.close();
 				}
@@ -304,7 +305,7 @@ public class Files{
 	
 	@Nullable
 	@SuppressWarnings("unchecked")
-	public static <T> List<T> readObjects(@NonNull File file){
+	public static <T> List<T> readObjects(@NonNull File file) {
 		
 		if (file.exists()) {
 			
@@ -320,7 +321,7 @@ public class Files{
 			}
 			finally {
 				
-				if(in != null) {
+				if (in != null) {
 					try {
 						in.close();
 					}
@@ -333,8 +334,6 @@ public class Files{
 		
 		return null;
 	}
-   
-	
 	
 	
 }
