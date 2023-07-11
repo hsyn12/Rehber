4e38412406fa6b83106ffd6bb50e3025755e1c9f
xyz <hsyntr33@gmail.com>
2023-07-11 09:44:44 +0300

    update codes

--- gate/src/main/java/com/tr/hsyn/gate/Gate.java
+++ gate/src/main/java/com/tr/hsyn/gate/Gate.java
@@ -8,7 +8,43 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * Gate.<br>
- * Entry-output point.
+ * Entry-output point.<br>
+ * This interface defines a logical gate that can be opened and closed.<br>
+ * So this logic has two states: open and closed.<br>
+ * Corresponding to these states, the logic has two actions: enter and exit.<br>
+ * This interface defines only three methods:
+ * {@link #isOpen()}, {@link #enter()} and {@link #exit()}.
+ * To be able to enter the gate, the gate must be opened.<br>
+ * {@link #isOpen()} checks if the gate is open.
+ * But not needed to check if the gate is open,
+ * the call {@link #enter()} is enough and returns {@code true} if the gate is open.
+ * If {@link #enter()} returns {@code true} means entering is successful,
+ * and the gate gets closed.<br>
+ * The gate keep closed until {@link #exit()} is called.<br><br>
+ *
+ * <pre>
+ * var gate = Gate.newGate();
+ *
+ * if (gate.enter())
+ *    System.out.println("Entering is successful.");
+ *
+ * if (gate.enter())
+ *     System.out.println("Trying to enter");
+ * else {
+ *
+ *     if (!gate.isOpen()) {
+ *
+ *        System.out.println("Gate is closed.");
+ *        gate.exit();
+ *
+ *        if (gate.enter())
+ *            System.out.println("Entering is successful again");
+ *    }
+ * }
+ * //Entering is successful.
+ * //Gate is closed.
+ * //Entering is successful again
+ * </pre>
  */
 public interface Gate {
 	
@@ -40,6 +76,29 @@ public interface Gate {
 	static Gate newGate() {
 		
 		return new SimpleGate();
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		var gate = Gate.newGate();
+		
+		if (gate.enter())
+			System.out.println("Entering is successful.");
+		
+		if (gate.enter())
+			System.out.println("Trying to enter");
+		else {
+			
+			if (!gate.isOpen()) {
+				
+				System.out.println("Gate is closed.");
+				gate.exit();
+				
+				if (gate.enter())
+					System.out.println("Entering is successful again");
+			}
+		}
 	}
 	
 }
