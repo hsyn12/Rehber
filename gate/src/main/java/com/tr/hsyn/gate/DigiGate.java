09a7fe42c7b2c0d40eb12be84d9da7a2b6edc33f
xyz <hsyntr33@gmail.com>
2023-07-12 12:12:04 +0300

    update codes

--- gate/src/main/java/com/tr/hsyn/gate/DigiGate.java
+++ gate/src/main/java/com/tr/hsyn/gate/DigiGate.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Digital Gate.<br>
  * This interface extends {@link AutoGate},
- * and adds a cyclic structure and provides to execute a runnable at the exit.
+ * and adds a cyclic structure and provides to execute a runnable on the exit.
  */
 public interface DigiGate extends AutoGate {
 	
@@ -28,6 +28,27 @@ public interface DigiGate extends AutoGate {
 	void setOnExit(Runnable onExit);
 	
 	/**
+	 * This method works for a gate that has an automatic exit duration and an exit operation.
+	 * Normally, the gate keeps closed until {@link #exit()} is called,
+	 * or until exits automatically after a certain duration.
+	 * And during this closed state, all calls to the gate get rejected and returns {@code false}.<br>
+	 * For example, say we have a gate that 3 seconds interval for the automatic exit.
+	 * While the gate is closed,
+	 * say the method {@link #enter()} is called when the automatic exit duration is left 1 second.
+	 * In this situation, the call gets rejected.
+	 * And that's it.
+	 * After one second, the gate will open automatically.
+	 * But before this scenario, if this method is called,
+	 * when the {@link #enter()} is called,
+	 * the interval duration is reset to 3 seconds.
+	 * This means that the gate will keep closed until 3 seconds more,
+	 * and the exit operation also will be delayed by 3 seconds.
+	 * And all rejected calls will cause the same thing.<br>
+	 * This is the same thing as searching something by writing the character in the search box.
+	 * The search box waits for a specific duration to be pressed any character, and then search.
+	 * The search box would be inefficient if it starts to search immediately.
+	 * This method has the same logic too.
+	 *
 	 * @return this digital gate with loop structure
 	 */
 	DigiGate loop();
