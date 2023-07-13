8c3ba958b00b595201c64b7b19db04a1683d4554
xyz <hsyntr33@gmail.com>
2023-07-13 15:01:50 +0300

    update codes

--- reflection/src/main/java/com/tr/hsyn/reflection/Clazz.java
+++ reflection/src/main/java/com/tr/hsyn/reflection/Clazz.java
@@ -34,12 +34,12 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Verilen {@linkplain Class} sınıfından nesne oluştur.
+	 * Creates an object from the given {@linkplain Class} class.
 	 *
-	 * @param clazz      Sınıf
-	 * @param parameters Kurucu argümanları
-	 * @param <T>        Sınıf türü
-	 * @return Nesne
+	 * @param clazz      class to create an object from
+	 * @param parameters parameters for constructor
+	 * @param <T>        type of the object
+	 * @return object created from the given {@linkplain Class}
 	 */
 	@Nullable
 	public static <T> T create(@NotNull final Class<T> clazz, @Nullable final Object... parameters) {
@@ -56,13 +56,12 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Sınıfın kurucusunu bul.
-	 * Sadece public.
+	 * Finds the constructor of the given {@linkplain Class}.
 	 *
-	 * @param clazz          Sınıf
-	 * @param parameterTypes Kurucunun parametre türleri
-	 * @param <T>            Sınıf türü
-	 * @return Kurucu
+	 * @param clazz          class
+	 * @param parameterTypes parameter types of the constructor
+	 * @param <T>            type of the object
+	 * @return {@linkplain Constructor}
 	 */
 	@Nullable
 	public static <T> Constructor<T> findConstructor(@NotNull Class<T> clazz, @Nullable Class<?>... parameterTypes) {
@@ -78,15 +77,15 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Metot ara.
-	 * Aranan metot private olabilir.
-	 * Arama, sınıfın üst sınıflarında da yapılır.
-	 * Static metotlar hariç.
+	 * Search method.
+	 * The method can be private.
+	 * The search is also done in the superclasses of the class.
+	 * Except for static methods.
 	 *
-	 * @param clazz          Sınıf
-	 * @param methodName     Aranan metodun adı
-	 * @param parameterTypes Aranan metodun parametre türleri
-	 * @param <T>            Sınıf türü
+	 * @param clazz          class to search the method in
+	 * @param methodName     name of the method
+	 * @param parameterTypes types of the method
+	 * @param <T>            type of the object
 	 * @return {@linkplain Method}
 	 */
 	@Nullable
@@ -104,16 +103,28 @@ public final class Clazz {
 		return null;
 	}
 	
+	/**
+	 * Search method.
+	 * The method can be private.
+	 * The search is also done in the superclasses of the class.
+	 * Except for static methods.
+	 *
+	 * @param clazz      class to search the method in
+	 * @param methodName name of the method
+	 * @param args       arguments of the method
+	 * @param <T>        type of the object
+	 * @return {@linkplain Method}
+	 */
 	public static <T> Method findMethod(@NotNull Class<? super T> clazz, @NotNull String methodName, @Nullable Object... args) {
 		
-		return findMethod(clazz, methodName, convertTypes(args));
+		return findMethod(clazz, methodName, convertToTypes(args));
 	}
 	
 	/**
-	 * Metot ara.
-	 * Aranan metot private olabilir.
-	 * Arama, sınıfın üst sınıflarında yapılmaz.
-	 * Static metotlar aranmaz.
+	 * Search method.
+	 * The method can be private.
+	 * The search is not done in the superclasses of the class.
+	 * Static methods are not included.
 	 *
 	 * @param clazz          Sınıf
 	 * @param methodName     Aranan metodun adı
@@ -131,21 +142,20 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Metot ara.
-	 * Aranan metot private olabilir.
-	 * Arama, sınıfın üst sınıflarında da yapılır.
-	 * Static metotlar aranmaz.
+	 * Search method.
+	 * The method can be private.
+	 * The search is also done in the superclasses of the class.
+	 * Except for static methods.
 	 *
-	 * @param clazzs         Nesne
-	 * @param methodName     Aranan metodun adı
-	 * @param parameterTypes Aranan metodun parametre türleri
-	 * @param <T>            Sınıf türü
+	 * @param object         object to search the method in
+	 * @param methodName     name of the method
+	 * @param parameterTypes types of the method parameters
 	 * @return {@linkplain Method}
 	 */
 	@Nullable
-	public static <T> Method findMethod(@NotNull Object clazzs, @NotNull String methodName, @Nullable Class<?>... parameterTypes) {
+	public static Method findMethod(@NotNull Object object, @NotNull String methodName, @Nullable Class<?>... parameterTypes) {
 		
-		Class<?> clazz = clazzs.getClass();
+		Class<?> clazz = object.getClass();
 		
 		do {
 			
@@ -160,21 +170,21 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Metot ara.
-	 * Aranan metot private olabilir.
-	 * Arama, sınıfın üst sınıflarında yapılmaz.
-	 * Static metotlar aranmaz.
+	 * Search method.
+	 * The method can be private.
+	 * The search is not done in the superclasses of the class.
+	 * Static methods are not included.
 	 *
-	 * @param clazzs         Nesne
+	 * @param object         object to search the method in
 	 * @param methodName     Aranan metodun adı
 	 * @param parameterTypes Aranan metodun parametre türleri
 	 * @param <T>            Sınıf türü
 	 * @return {@linkplain Method}
 	 */
 	@Nullable
-	public static <T> Method findMethodThis(@NotNull Object clazzs, @NotNull String methodName, @Nullable Class<?>... parameterTypes) {
+	public static <T> Method findMethodThis(@NotNull Object object, @NotNull String methodName, @Nullable Class<?>... parameterTypes) {
 		
-		Class<?> clazz = clazzs.getClass();
+		Class<?> clazz = object.getClass();
 		
 		try {return clazz.getDeclaredMethod(methodName, parameterTypes);}
 		catch (NoSuchMethodException ignored) {}
@@ -183,17 +193,17 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Verilen sınıfın, verilen annotation sınıfı ile
-	 * işaretlenmiş tüm metotlarını döndür.
-	 * Private metotlar dahil.
-	 * Sınıfın üst sınıfları da dahil.
-	 * Static metotlar hariç.
+	 * Returns all methods of the given class
+	 * that are marked with the given annotation class.
+	 * Including private methods.
+	 * Including the upper classes.
+	 * Not including static methods.
 	 *
-	 * @param clazz           Sınıf
-	 * @param annotationClazz Annotation sınıfı
-	 * @param <A>             Annotation türü
-	 * @param <T>             Sınıf türü
-	 * @return Metot listesi
+	 * @param clazz           class
+	 * @param annotationClazz Annotation class
+	 * @param <A>             Annotation type
+	 * @param <T>             class type
+	 * @return List of methods
 	 */
 	@NotNull
 	public static <A extends Annotation, T> List<Method> getMethodsAnnotated(@NotNull Class<? super T> clazz, @NotNull Class<A> annotationClazz) {
@@ -210,17 +220,16 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Verilen sınıfın, verilen annotation sınıfı ile
-	 * işaretlenmiş tüm metotlarını döndür.
-	 * Private metotlar dahil.
-	 * Sınıfın üst sınıfları dahil değil.
-	 * Static metotlar dahil değil.
+	 * Return all methods of the given class that are marked with the given annotation class.
+	 * Including private methods.
+	 * The superclasses of the class are not included.
+	 * Static methods are not included.
 	 *
-	 * @param clazz           Sınıf
-	 * @param annotationClazz Annotation sınıfı
-	 * @param <A>             Annotation türü
-	 * @param <T>             Sınıf türü
-	 * @return Metot listesi
+	 * @param clazz           class
+	 * @param annotationClazz Annotation class
+	 * @param <A>             Annotation type
+	 * @param <T>             class type
+	 * @return List of methods
 	 */
 	public static <A extends Annotation, T> List<Method> getMethodsAnnotatedThis(@NotNull Class<T> clazz, @NotNull Class<A> annotationClazz) {
 		
@@ -230,14 +239,14 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Tüm metotları al.
-	 * Private metotlar dahil.
-	 * Sınıfın üst sınıfları da dahil.
-	 * Static metotlar hariç.
+	 * Returns all methods.
+	 * Including private methods.
+	 * Including the super classes.
+	 * Except for static methods.
 	 *
-	 * @param clazz Sınıf
-	 * @param <T>   Sınıf türü
-	 * @return Metot listesi
+	 * @param clazz class
+	 * @param <T>   class type
+	 * @return List of methods
 	 */
 	@NotNull
 	public static <T> List<Method> getMethods(@NotNull Class<? super T> clazz) {
@@ -253,14 +262,13 @@ public final class Clazz {
 	}
 	
 	/**
-	 * Tüm metotları al.
-	 * Private metotlar dahil.
-	 * Sınıfın üst sınıfları dahil değil.
-	 * Static metotlar dahil değil.
+	 * Get all methods. Including private methods.
+	 * The superclasses of the class are not included.
+	 * Static methods are not included.
 	 *
-	 * @param clazz Sınıf
-	 * @param <T>   Sınıf türü
-	 * @return Metot listesi
+	 * @param clazz class
+	 * @param <T>   class type
+	 * @return List of methods
 	 */
 	@NotNull
 	public static <T> List<Method> getMethodsThis(@NotNull final Class<T> clazz) {
@@ -268,12 +276,28 @@ public final class Clazz {
 		return new LinkedList<>(Arrays.asList(clazz.getDeclaredMethods()));
 	}
 	
+	/**
+	 * Invokes the method.
+	 *
+	 * @param method method to invoke
+	 * @param object object that has the method
+	 * @param args   arguments of the method
+	 */
 	public static void invoke(@NotNull Method method, @Nullable Object object, @Nullable Object... args) {
 		
 		try {method.invoke(object, args);}
 		catch (Exception e) {e.printStackTrace();}
 	}
 	
+	/**
+	 * Invokes the method.
+	 *
+	 * @param object object that has the method
+	 * @param method method to invoke
+	 * @param args   arguments of the method
+	 * @param <T>    return type
+	 * @return the method return value
+	 */
 	@Nullable
 	@SuppressWarnings("unchecked")
 	public static <T> T invoke(@Nullable Object object, @NotNull Method method, @Nullable Object... args) {
@@ -284,13 +308,28 @@ public final class Clazz {
 		return null;
 	}
 	
+	/**
+	 * Returns the annotation.
+	 *
+	 * @param element         AnnotatedElement
+	 * @param annotationClass Annotation class
+	 * @param <T>             Annotation type
+	 * @return Annotation
+	 */
 	@Nullable
 	public static <T extends Annotation> T getAnnotation(@NotNull AnnotatedElement element, @NotNull Class<T> annotationClass) {
 		
 		return element.getAnnotation(annotationClass);
 	}
 	
-	//@SafeVarargs
+	/**
+	 * Returns the annotations.
+	 *
+	 * @param element     AnnotatedElement
+	 * @param annotations Annotation class list
+	 * @param <T>         Annotation type
+	 * @return List of annotations
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T extends Annotation> List<T> getAnnotations(AnnotatedElement element, @Nullable Class<T>... annotations) {
 		
@@ -300,24 +339,62 @@ public final class Clazz {
 		return new ArrayList<>(Arrays.asList((T[]) element.getAnnotations()));
 	}
 	
+	/**
+	 * Get declared fields.
+	 * Includes public, protected, default (package) access, and private fields,
+	 * but excludes inherited fields.
+	 * If the Class object represents a class or interface with no declared fields,
+	 * then this method returns an empty list.
+	 *
+	 * @param clazz    class to get the fields
+	 * @param modifier field modifier to filter
+	 * @return List of fields
+	 */
 	public static List<Field> getDeclaredFields(@NotNull Class<?> clazz, int modifier) {
 		
 		return Arrays.stream(clazz.getDeclaredFields()).filter(f -> (f.getModifiers() & modifier) == modifier).collect(Collectors.toList());
 	}
 	
+	/**
+	 * Get declared fields.
+	 * Includes public, protected, default (package) access, and private fields,
+	 * but excludes inherited fields.
+	 * If the Class object represents a class or interface with no declared fields,
+	 * then this method returns an empty list.
+	 *
+	 * @param clazz class to get the fields
+	 * @param type  field type to filter
+	 * @return List of fields
+	 */
 	public static List<Field> getDeclaredFields(@NotNull Class<?> clazz, Class<?> type) {
 		
 		return Arrays.stream(clazz.getDeclaredFields()).filter(f -> f.getType() == type).collect(Collectors.toList());
 	}
 	
+	/**
+	 * Get declared fields.
+	 * Includes public, protected, default (package) access, and private fields,
+	 * but excludes inherited fields.
+	 * If the Class object represents a class or interface with no declared fields,
+	 * then this method returns an empty list.
+	 *
+	 * @param clazz class to get the fields
+	 * @return List of fields
+	 */
 	public static List<Field> getDeclaredFields(@NotNull Class<?> clazz) {
 		
 		return Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList());
 	}
 	
+	/**
+	 * Converts the given arguments to an array of types.
+	 *
+	 * @param args arguments
+	 * @return array of types of the arguments, which are in the same order
+	 */
 	@NotNull
 	@Contract(pure = true)
-	public static Class<?>[] convertTypes(Object... args) {
+	public static Class<?>[] convertToTypes(Object... args) {
 		
 		if (args.length == 0) return new Class<?>[0];
 		
@@ -344,6 +421,13 @@ public final class Clazz {
 		
 	}
 	
+	/**
+	 * Returns the value of the static field.
+	 *
+	 * @param clazz     class
+	 * @param fieldName field name
+	 * @return value of the field
+	 */
 	public static int getIntStatic(@NotNull Class<?> clazz, @NotNull String fieldName) {
 		
 		try {
@@ -360,6 +444,13 @@ public final class Clazz {
 		return Integer.MAX_VALUE;
 	}
 	
+	/**
+	 * Returns the value of the field.
+	 *
+	 * @param o         object to get the field
+	 * @param fieldName field name
+	 * @return value of the field
+	 */
 	public static int getInt(@NotNull Object o, @NotNull String fieldName) {
 		
 		try {
@@ -377,4 +468,55 @@ public final class Clazz {
 		return Integer.MAX_VALUE;
 	}
 	
+	/**
+	 * Returns the value of the field.
+	 *
+	 * @param o         object to get the field
+	 * @param fieldName field name
+	 * @param <T>       type of the field
+	 * @return value of the field
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> @Nullable T getFieldValue(@NotNull Object o, @NotNull String fieldName) {
+		
+		try {
+			
+			Field field = o.getClass().getDeclaredField(fieldName);
+			
+			field.setAccessible(true);
+			
+			return (T) field.get(o);
+		}
+		catch (IllegalAccessException | NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Returns the value of the static field.
+	 *
+	 * @param clazz     class to get the field
+	 * @param fieldName field name
+	 * @param <T>       type of the field
+	 * @return value of the field
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> @Nullable T getFieldValue(@NotNull Class<?> clazz, @NotNull String fieldName) {
+		
+		try {
+			
+			Field field = clazz.getDeclaredField(fieldName);
+			
+			field.setAccessible(true);
+			
+			return (T) field.get(null);
+		}
+		catch (IllegalAccessException | NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 }
