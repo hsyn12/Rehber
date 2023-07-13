ffb24fddbcbd203cbe7e9a1794e8a04534bcaad6
xyz <hsyntr33@gmail.com>
2023-07-13 14:25:30 +0300

    update codes

--- random/src/main/java/com/tr/hsyn/random/Randoom.java
+++ random/src/main/java/com/tr/hsyn/random/Randoom.java
@@ -2,98 +2,98 @@ package com.tr.hsyn.random;
 
 
 import java.security.SecureRandom;
-import java.util.Random;
 
 
 /**
- * Rastgele sayı veya boolean değer üreten metotlar sağlar.
+ * It provides methods that generate random numbers or boolean values.
  */
 public class Randoom {
-
-    private static final SecureRandom RANDOM = new SecureRandom();
-
-    /**
-     * @return Rastgele {@code true} yada {@code false}
-     */
-    public static boolean getBool() {
-
-        return RANDOM.nextBoolean();
-    }
-
-    /**
-     * Yüzde ile bir şans ver.
-     *
-     * @param percent 100 yada 100'den büyük olursa kesinlikle {@code true} döner. 0 yada 0'dan küçük olursa kesinlikle {@code false} döner
-     * @return Verilen yüzdeye göre bir şans
-     */
-    public static boolean getBool(int percent) {
-
-        if (percent >= 100) return true;
-        if (percent <= 0) return false;
-
-        return getInt(100 / percent) == 0;
-    }
-
-    /**
-     * Rastgele bir sayı.
-     *
-     * @param end Bitiş sınırı. (Sayıya dahil değil)
-     * @return [0, end) aralığında bir sayı
-     */
-    public static int getInt(int end) {
-
-        return RANDOM.nextInt(end);
-    }
-
-    /**
-     * @return Random integer
-     */
-    public static int getInt() {
-
-        return RANDOM.nextInt();
-    }
-
-    /**
-     * İki sayı aralığında bir sayı üretir.
-     *
-     * @param start İlk sayı başlangıç sınırı. Üretilecek sayıya dahildir.
-     * @param end   İkinci sayı bitiş sınırı. Üretilecek sayıya dahil değildir.
-     * @return İki sayı aralığında rastgele bir sayı
-     */
-    public static int getInt(int start, int end) {
-
-        return getInt(end - start) + start;
-    }
-
-    /**
-     * @return Random long
-     */
-    public static long getLong() {
-
-        return RANDOM.nextLong();
-    }
-
-    /**
-     * İki sayı aralığında bir sayı üretir.
-     *
-     * @param start İlk sayı başlangıç sınırı. Üretilecek sayıya dahildir.
-     * @param end   İkinci sayı bitiş sınırı. Üretilecek sayıya dahil değildir.
-     * @return İki sayı aralığında rastgele bir sayı
-     */
-    public static long getLong(long start, long end) {
-
-        return getLong(end - start) + start;
-    }
-
-    /**
-     * Rastgele bir long sayı döndürür.
-     *
-     * @param end Bitiş sınırı. Sayıya dahil değil
-     * @return [0, end) aralığında bir sayı. Sıfır dahil
-     */
-    public static long getLong(long end) {
-
-        return Math.abs(RANDOM.nextLong()) % end;
-    }
-
+	
+	private static final SecureRandom RANDOM = new SecureRandom();
+	
+	/**
+	 * @return random {@code true} or {@code false}
+	 */
+	public static boolean getBool() {
+		
+		return RANDOM.nextBoolean();
+	}
+	
+	/**
+	 * Give it a shot with a percentage.
+	 *
+	 * @param percent If it is 100 or greater than 100 it will definitely return {@code true}.
+	 *                If it is 0 or less than 0 it will definitely return {@code false}
+	 * @return {@code true} or {@code false} depending on the percentage.
+	 */
+	public static boolean getBool(int percent) {
+		
+		if (percent >= 100) return true;
+		if (percent <= 0) return false;
+		
+		return getInt(100 / percent) == 0;
+	}
+	
+	/**
+	 * Random number.
+	 *
+	 * @param end End limit. (Not included)
+	 * @return [0, end) a number in the range
+	 */
+	public static int getInt(int end) {
+		
+		return RANDOM.nextInt(end);
+	}
+	
+	/**
+	 * @return Random number
+	 */
+	public static int getInt() {
+		
+		return RANDOM.nextInt();
+	}
+	
+	/**
+	 * Generates a number in the range of two numbers.
+	 *
+	 * @param start The first number is the starting limit. It is included in the number to be produced.
+	 * @param end   Second number ending limit. It is not included in the number to be produced.
+	 * @return A random number in the range of two numbers
+	 */
+	public static int getInt(int start, int end) {
+		
+		return getInt(end - start) + start;
+	}
+	
+	/**
+	 * @return Random long
+	 */
+	public static long getLong() {
+		
+		return RANDOM.nextLong();
+	}
+	
+	/**
+	 * Generates a number in the range of two numbers.
+	 *
+	 * @param start The first number is the starting limit. It is included in the number to be produced.
+	 * @param end   Second number ending limit. It is not included in the number to be produced.
+	 * @return A random number in the range of two numbers
+	 */
+	public static long getLong(long start, long end) {
+		
+		return getLong(end - start) + start;
+	}
+	
+	/**
+	 * Random number.
+	 *
+	 * @param end End limit. (Not included)
+	 * @return [0, end) a number in the range
+	 */
+	public static long getLong(long end) {
+		
+		return Math.abs(RANDOM.nextLong()) % end;
+	}
+	
 }
